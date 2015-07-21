7a0c149b775dbeb538fad361496ec1d76bb57148
ajaymellon <ajaymellon@gmail.com>
2015-07-21 11:07:24 +0530

    Initial Commit

---
+++ CustomTabBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomTabBar
+//
+//  Created by Ajay Jha on 21/07/15.
+//  Copyright (c) 2015 Ajay Jha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomTabBar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomTabBar
+//
+//  Created by Ajay Jha on 21/07/15.
+//  Copyright (c) 2015 Ajay Jha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
